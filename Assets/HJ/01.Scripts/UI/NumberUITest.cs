cef492e834ff60dd7d8c2eb90e51f568a48e9ca1
kimhijin <ja92bab@gmail.com>
2025-12-24 02:14:42 +0900

    feat:sound

--- Assets/HJ/01.Scripts/UI/NumberUITest.cs
+++ Assets/HJ/01.Scripts/UI/NumberUITest.cs
@@ -12,6 +12,8 @@ public class NumberUITest : MonoBehaviour
 
     private void Start()
     {
+        SoundManager.Instance.PlaySFX("NumberCount");
+
         Time.timeScale = 0;
         one.transform.localScale = new Vector2(0, 0);
         two.transform.localScale = new Vector2(0, 0);
@@ -20,13 +22,13 @@ public class NumberUITest : MonoBehaviour
 
         sq = DOTween.Sequence();
         sq.SetUpdate(true);
-        sq.Append(one.rectTransform.DOScale(new Vector2(1,1), 0.5f));
-        sq.Append(two.rectTransform.DOScale(new Vector2(1, 1), 0.5f))
-            .Join(one.DOFade(0,0.5f));
-        sq.Append(three.rectTransform.DOScale(new Vector2(1,1), 0.5f))
-            .Join(two.DOFade(0, 0.5f)) ;
-        sq.Append(go.rectTransform.DOScale(new Vector2(1,1), 0.6f).SetEase(Ease.OutCubic))
-            .Join(three.DOFade(0, 0.5f));
+        sq.Append(one.rectTransform.DOScale(new Vector2(1,1), 1f));
+        sq.Append(two.rectTransform.DOScale(new Vector2(1, 1), 1f))
+            .Join(one.DOFade(0,0.6f));
+        sq.Append(three.rectTransform.DOScale(new Vector2(1,1), 1f))
+            .Join(two.DOFade(0, 0.6f)) ;
+        sq.Append(go.rectTransform.DOScale(new Vector2(1,1), 1f).SetEase(Ease.OutCubic))
+            .Join(three.DOFade(0, 0.6f));
         sq.Append(go.DOFade(0, 0.6f));
         sq.OnComplete(StartGame);
 
